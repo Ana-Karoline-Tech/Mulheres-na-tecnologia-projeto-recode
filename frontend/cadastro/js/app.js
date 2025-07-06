d17551a1715606a0accb148c771b29fa5df4b786
Ana-Karoline-Tech <anna.karolinawork@gmail.com>
2025-07-06 03:54:38 -0300

    Atualizações na tela de login

--- frontend/cadastro/js/app.js
+++ frontend/cadastro/js/app.js
@@ -1,12 +1,13 @@
-var btnSignin = document.querySelector("#signin");
-var btnSignup = document.querySelector("#signup");
-
-var body = document.querySelector("body");
+const btnSignin = document.querySelector("#signin");  // Botão ALUNAS
+const btnSignup = document.querySelector("#signup");  // Botão EMPRESAS
+const body = document.querySelector("body");
 
 btnSignin.addEventListener("click", function () {
-  body.className = "sign-in-js";
+    body.classList.remove("conteudo-cadastro-js");
+    body.classList.add("conteudo-login-js");
 });
 
 btnSignup.addEventListener("click", function () {
-  body.className = "sign-up-js";
+    body.classList.remove("conteudo-login-js");
+    body.classList.add("conteudo-cadastro-js");
 });
